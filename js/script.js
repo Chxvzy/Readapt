a1a8fabd1f80cb0ef821421f9c6c87c959ca8352
Felipe Lorenzo <xzy.sung@gmail.com>
2026-07-24 02:05:13 -0300

    ADD: Initial dark mode setup and switch transition effect.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 const menuBtn = document.querySelector('.menu-btn');
 const overlay = document.querySelector('.menu-overlay');
+const themeSwitch = document.querySelector('#theme-switch');
 
 menuBtn.addEventListener('click', () => {
-    overlay.classList.toggle('active');
 
+    overlay.classList.toggle('active');
     document.body.classList.toggle('menu-open');
-    
+
+});
+
+if(localStorage.getItem('theme') === 'dark'){
+
+    document.body.classList.add('dark');
+
+}
+
+themeSwitch.addEventListener('click', () => {
+
+    document.body.classList.toggle('dark');
+
+    if(document.body.classList.contains('dark')){
+
+        localStorage.setItem('theme', 'dark');
+
+    }else{
+
+        localStorage.setItem('theme', 'light');
+
+    }
+
 });
