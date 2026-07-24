040e2c0f87ecdf06f061e316955001b5245f695e
Felipe Lorenzo <xzy.sung@gmail.com>
2026-07-24 02:58:32 -0300

    refactor: dark mode structure and dynamic logo switching.

--- js/script.js
+++ js/script.js
@@ -1,32 +1,26 @@
 const menuBtn = document.querySelector('.menu-btn');
 const overlay = document.querySelector('.menu-overlay');
 const themeSwitch = document.querySelector('#theme-switch');
+const logo = document.querySelector('#logo-img'); 
 
 menuBtn.addEventListener('click', () => {
-
     overlay.classList.toggle('active');
     document.body.classList.toggle('menu-open');
-
 });
 
-if(localStorage.getItem('theme') === 'dark'){
-
+if (localStorage.getItem('theme') === 'dark') {
     document.body.classList.add('dark');
-
+    logo.src = 'assets/logo-readapt-white.png'; 
 }
 
 themeSwitch.addEventListener('click', () => {
-
     document.body.classList.toggle('dark');
 
-    if(document.body.classList.contains('dark')){
-
+    if (document.body.classList.contains('dark')) {
         localStorage.setItem('theme', 'dark');
-
-    }else{
-
+        logo.src = 'assets/logo-readapt-white.png'; 
+    } else {
         localStorage.setItem('theme', 'light');
-
+        logo.src = 'assets/readapt-logo.png'; 
     }
-
 });
