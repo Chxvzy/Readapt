ceb4b05d72110dfbf0d484d041cabffd54dfd9a0
Felipe Lorenzo <xzy.sung@gmail.com>
2026-09-02 01:37:24 -0300

    Ajustes feitos na pagina HOME modelo desktop

--- js/script.js
+++ js/script.js
@@ -1,52 +1,49 @@
 const menuBtn = document.querySelector('.menu-btn');
 const overlay = document.querySelector('.menu-overlay');
 const themeSwitch = document.querySelector('#theme-switch');
-const logo = document.querySelector('#logo-img');
+
+const logoMobile = document.querySelector('#logo-mobile');
+const logoDesktop = document.querySelector('#logo-desktop');
 
 function updateThemeIcons() {
-
     const dark = document.body.classList.contains('dark');
 
     document.querySelectorAll('.theme-icon').forEach(icon => {
-
         if (dark) {
-
             icon.src = icon.src
                 .replace('-black.svg', '-white.svg')
                 .replace('-black.png', '-white.png');
-
         } else {
-
             icon.src = icon.src
                 .replace('-white.svg', '-black.svg')
                 .replace('-white.png', '-black.png');
-
         }
-
     });
-
 }
 
+// Menu mobile
 menuBtn.addEventListener('click', () => {
-
     overlay.classList.toggle('active');
     document.body.classList.toggle('menu-open');
-
 });
 
+// Verifica o tema salvo
 if (localStorage.getItem('theme') === 'dark') {
 
     document.body.classList.add('dark');
-    logo.src = 'assets/logo-readapt-white.png';
+
+    logoMobile.src = 'assets/logo-readapt-white.png';
+    logoDesktop.src = 'assets/logo-readapt-white.png';
 
 } else {
 
-    logo.src = 'assets/readapt-logo.png';
-
+    logoMobile.src = 'assets/readapt-logo.png';
+    logoDesktop.src = 'assets/readapt-logo.png';
 }
 
 updateThemeIcons();
 
+// Alternar tema
 themeSwitch.addEventListener('click', () => {
 
     document.body.classList.toggle('dark');
@@ -54,16 +51,17 @@ themeSwitch.addEventListener('click', () => {
     if (document.body.classList.contains('dark')) {
 
         localStorage.setItem('theme', 'dark');
-        logo.src = 'assets/logo-readapt-white.png';
+
+        logoMobile.src = 'assets/logo-readapt-white.png';
+        logoDesktop.src = 'assets/logo-readapt-white.png';
 
     } else {
 
         localStorage.setItem('theme', 'light');
-        logo.src = 'assets/readapt-logo.png';
 
+        logoMobile.src = 'assets/readapt-logo.png';
+        logoDesktop.src = 'assets/readapt-logo.png';
     }
 
     updateThemeIcons();
-
 });
-
